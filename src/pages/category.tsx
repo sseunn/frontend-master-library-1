14526ecb35a5f2a2c0a04238f09dfce778f88ebe
김선주 <sseunn123@naver.com>
2022-04-23 21:51:31 +0900

    디테일페이지 레이아웃 완성

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
-
-function category() {
-  return <div>category</div>;
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 80%;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: salmon;
+`;
+const GridContainer = styled.div`
+  display: grid;
+  width: 100%;
+  justify-content: center;
+  background-color: white;
+  gap: 5px;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+`;
+const GridItem = styled.div`
+  background-color: black;
+  height: 30rem;
+  &:hover {
+    background-color: white;
+  }
+`;
+interface BookListProps {
+  id: string;
+  bookTitle: string;
 }
-
-export default category;
+function Category() {
+  const [bookList, setBookList] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/data/bookList.json", {})
+      .then((response) => response.json())
+      .then((data) => setBookList(data.bookList));
+  }, []);
+  console.log(bookList);
+  return (
+    <div>
+      {" "}
+      <Section>
+        {" "}
+        Category{" "}
+        <GridContainer>
+          {" "}
+          {/* {bookList.map((el) => ( <GridItem key={el.id}>{el.bookTitle}</GridItem> ))} */}{" "}
+          <GridItem>dd</GridItem> <GridItem>dd</GridItem>{" "}
+          <GridItem>dd</GridItem> <GridItem>dd</GridItem>{" "}
+          <GridItem>dd</GridItem>{" "}
+        </GridContainer>{" "}
+      </Section>{" "}
+    </div>
+  );
+}
+export default Category;
--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,7 +1,122 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
 function detail() {
-  return <div>detail</div>;
+  return (
+    <div>
+      <Container>
+        <FlexWrapper>
+          <Left>
+            <Cover></Cover>
+          </Left>
+
+          <Right>
+            <Title>ddd</Title>
+            <SubTitle>kkk</SubTitle>
+            <Writer>저자</Writer>
+            <Buylink>
+              <Kyobo>교보문고</Kyobo>
+              <Yes24>Yes24</Yes24>
+              <Interpark>인터파크</Interpark>
+            </Buylink>
+          </Right>
+        </FlexWrapper>
+        <DownWrapper>
+          <Description>책 소개</Description>
+          <Comment>댓글</Comment>
+        </DownWrapper>
+      </Container>
+    </div>
+  );
 }
 
 export default detail;
+
+const Container = styled.div`
+  height: auto;
+  width: 100%;
+  background-color: #b2bec3;
+  margin-top: 82px;
+`;
+
+const FlexWrapper = styled.div`
+  display: flex;
+  width: auto;
+  height: auto;
+  margin: 3%;
+  padding: 3%;
+`;
+
+const Left = styled.div`
+  background-color: #b2bec3;
+  border: 2px solid black;
+  width: 15rem;
+  height: 20rem;
+`;
+
+const Cover = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: black;
+`;
+
+const Right = styled.div`
+  border: 1px solid black;
+  width: 80%;
+  height: 80%;
+  margin: 5%;
+  padding: 2%;
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 3rem;
+`;
+const SubTitle = styled.div`
+  font-size: 1.8rem;
+  display: flex;
+  margin-top: 10px;
+`;
+
+const Buylink = styled.div`
+  font-size: 1rem;
+  display: flex;
+  margin-top: 10px;
+`;
+
+const Kyobo = styled.div`
+  font-size: 1rem;
+`;
+
+const Yes24 = styled.div`
+  font-size: 1rem;
+`;
+
+const Interpark = styled.div`
+  font-size: 1rem;
+`;
+
+const Writer = styled.div`
+  font-weight: bold;
+`;
+
+const DownWrapper = styled.div`
+  font-size: 1rem;
+  margin-left: 5%;
+`;
+
+const Description = styled.div`
+  font-size: 2rem;
+  margin-bottom: 5%;
+  padding-left: 30px;
+  width: 100%;
+  height: 100%;
+`;
+
+const Comment = styled.div`
+  background-color: #b2bec3;
+  border-top: 1px solid black;
+  padding-top: 30px;
+  padding-left: 30px;
+  height: 20rem;
+`;
